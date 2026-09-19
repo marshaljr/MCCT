66e13e15d9b26ce400693322e6c0fec7810f8c94
Marshal Ram <84084013+marshaljr@users.noreply.github.com>
2026-09-19 22:03:26 +0545

    print format

---
+++ Python/day6.py
@@ -0,0 +1,17 @@
+units_used = 80
+rate = 15
+bill = units_used * rate
+
+print("======== ELECTRICITY BILL ========")
+print(f"   Units_Used : {units_used}")
+print(f"   Rate       : {rate}")
+print(f"   Bill       : {bill}")
+print("==================================")
+
+print ("\n======== STUDENT MARKS ========")
+
+print(f"{'Student':<12}{'Math':<8}{'Python':<8}")
+print("-" * 28)
+print(f"{'RAM':<12}{85:<8}{90:<8}")
+print(f"{'Sita':<12}{90:<8}{80:<8}")
+print(f"{'Anyone':<12}{78:<8}{88:<8}")

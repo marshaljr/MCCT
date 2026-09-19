f76d5522b0b9a6389b612a6ebe9df4368e82f8f7
Marshal Ram <84084013+marshaljr@users.noreply.github.com>
2026-09-19 21:59:22 +0545

    first commit

---
+++ Python/day2.py
@@ -0,0 +1,8 @@
+#variable;
+#It is a container that holds data. It can be used to store values, which can be changed during the execution of a program. Variables are essential in programming as they allow developers to manipulate and manage data effectively.
+
+#Naming conventions;
+#1. variable names should start with a letter or an underscore (_).
+#2. variable names can contain letters, numbers, and underscores.
+#3. variable names should not be a reserved keyword in the programming language.
+
